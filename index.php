f17293762c50cd1ab479ba992407822ed3aeb14e
Serg Prog <progserg.85@mail.ru>
2016-10-12 21:17:10 +0300

    передача сгенерированного массива через POST и выгрузка в исходную таблицу

--- index.php
+++ index.php
@@ -15,30 +15,55 @@
             <li>3</li>
         </ul>
     </div>
+    <?php if (empty($_POST['origMatrix'])) {
+        $i = 0;
+        while ($i < 9) {
+            $origMatrix[$i] = rand(0, 100);
+            $i++;
+        }
+        //echo 'origMatrix пустой';
+    } else {
+        $origMatrix = $_POST['origMatrix'];
+    } ?>
     <div id="matrix">
         <table border="solid">
             <th bgcolor="black" colspan="3">matrix</th>
-            <?php $x=0; while ($x<3): ?>
+            <?php $j = 0;
+            $x = 0;
+            while ($x < 3): ?>
             <tr>
-            <?php $y=0; while ($y<3): ?>
-            <td><?php $val=rand(0, 100); echo $val; ?></td>
-            <?php $y++; endwhile;?>
-            <?php $x++; endwhile;?>
+                <?php $y = 0;
+                while ($y < 3): ?>
+                    <td><? echo $origMatrix[$j];
+                        $j++; ?></td>
+                    <?php $y++; endwhile; ?>
+                <?php $x++;
+                endwhile;
+                ?>
             </tr>
         </table>
     </div>
     <div id="btn">
-        <input type="submit" value="==>">
+        <form action="index.php" method="post">
+            <?php foreach ($origMatrix as $elem): ?>
+                <input type="hidden" name=origMatrix[] value="<?php echo $elem; ?>">
+            <?php endforeach; ?>
+            <input type="submit" value="==>">
+        </form>
     </div>
     <div id="matrix">
         <table border="solid">
             <th bgcolor="black" colspan="3">matrix</th>
-            <?php $x=0; while ($x<3): ?>
+            <?php $x = 0;
+            while ($x < 3): ?>
             <tr>
-                <?php $y=0; while ($y<3): ?>
-                    <td><?php $val=rand(0, 100); echo $val ?></td>
-                    <?php $y++; endwhile;?>
-                <?php $x++; endwhile;?>
+                <?php $y = 0;
+                while ($y < 3): ?>
+                    <td><?php $val = rand(0, 100);
+                        echo $val ?></td>
+                    <?php $y++; endwhile; ?>
+                <?php $x++;
+                endwhile; ?>
             </tr>
         </table>
     </div>
